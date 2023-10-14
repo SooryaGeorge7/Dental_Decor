5ba2e8ed9bf17bfc12d3727a3577eafd4767e4d0
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-10-14 21:52:28 +0000

    feat:add product details template and urls

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shop_products, name='shop_products')
+    path('', views.shop_products, name='shop_products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
