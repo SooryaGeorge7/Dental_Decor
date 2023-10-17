0754166bdd7e1c42137725e619f4c75f24b4e3e3
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-10-17 15:11:12 +0000

    feat:added context.py,buttons in details

---
+++ shoppingbag/contexts.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+from django.conf import settings
+
+def shoppingbag_contents(request):
+    shoppingbag_items = []
+    total = 0
+    product_count = 0
+
+    
+    
+    context = {
+        'shoppingbag_items': shoppingbag_items,
+        'total': total,
+        'product_count': product_count,
+    }
+
+    return context
+
