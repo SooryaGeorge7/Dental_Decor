6abc90f83d9402e24fa573ef42acffe15ee6504e
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-10-18 11:34:01 +0000

    feat:updated product size functionality

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -10,12 +10,24 @@ def add_to_shoppingbag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     shoppingbag = request.session.get('shoppingbag', {})
 
-    if item_id in list(shoppingbag.keys()):
-        shoppingbag[item_id] += quantity
+    if size:
+        if item_id in list(shoppingbag.keys()):
+            if size in shoppingbag[item_id]['items_by_size'].keys():
+                shoppingbag[item_id]['items_by_size'][size] += quantity
+            else:
+                shoppingbag[item_id]['items_by_size'][size] = quantity
+        else:
+            shoppingbag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        shoppingbag[item_id] = quantity
+        if item_id in list(shoppingbag.keys()):
+            shoppingbag[item_id] += quantity
+        else:
+            shoppingbag[item_id] = quantity
 
     request.session['shoppingbag'] = shoppingbag
     print(request.session['shoppingbag'])
