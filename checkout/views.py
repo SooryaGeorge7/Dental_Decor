da73a18c60c089ed02b4985a838b5334b2d371cc
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-10-26 13:37:21 +0000

    feat:added stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O5RLNLXDIjnmjWheHiUGv8gpdHMztxkDgtdzfObaHWCHdsPUAUFQvrpzc3DzY1pI9I31BNXo0cO5lq2ihwvfEDg00U4BEMhwh',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
