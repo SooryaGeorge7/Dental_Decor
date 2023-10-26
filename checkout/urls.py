c77424af53d149fdfae6d8aff08cf36712baa87c
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-10-26 15:21:57 +0000

    feat:added basic checkout success feature

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='shop_checkout')
+    path('', views.checkout, name='shop_checkout'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
