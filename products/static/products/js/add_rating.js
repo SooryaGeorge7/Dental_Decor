fd0f7731359ae073080d89ec9fbdecdf5453bdcd
SooryaGeorge7 <sooryageorge07@gmail.com>
2023-11-14 21:49:43 +0000

    feat:added update_rating js

--- products/static/products/js/add_rating.js
+++ products/static/products/js/add_rating.js
@@ -8,34 +8,34 @@ var handleRatings = (choice, criteria) => {
     var stars = document.querySelectorAll(`.${criteria} .btn i`);
 
     console.log(stars)
-    
+
 
     stars.forEach(star => star.classList.remove('checked'));
-    
+
     var i;
     switch (choice) {
         case `${criteria}-first`:
-            for ( i = 0; i < 1; i++) {
+            for (i = 0; i < 1; i++) {
                 stars[i].classList.add('checked');
             }
             break;
         case `${criteria}-second`:
-            for ( i = 0; i < 2; i++) {
+            for (i = 0; i < 2; i++) {
                 stars[i].classList.add('checked');
             }
             break;
         case `${criteria}-third`:
-            for ( i = 0; i < 3; i++) {
+            for (i = 0; i < 3; i++) {
                 stars[i].classList.add('checked');
             }
             break;
         case `${criteria}-fourth`:
-            for ( i = 0; i < 4; i++) {
+            for (i = 0; i < 4; i++) {
                 stars[i].classList.add('checked');
             }
             break;
         case `${criteria}-fifth`:
-            for ( i = 0; i < 5; i++) {
+            for (i = 0; i < 5; i++) {
                 stars[i].classList.add('checked');
             }
             break;
@@ -49,15 +49,15 @@ var handleRatings = (choice, criteria) => {
 
 var getNumValue = (stringValue) => {
     let numValue;
-    if (stringValue.endsWith('-first')) {
+    if (stringValue.endsWith('-first') ) {
         numValue = 1;
-    } else if (stringValue.endsWith('-second')) {
+    } else if (stringValue.endsWith('-second') ) {
         numValue = 2;
-    } else if (stringValue.endsWith('-third')) {
+    } else if (stringValue.endsWith('-third') ){
         numValue = 3;
-    } else if (stringValue.endsWith('-fourth')) {
+    } else if (stringValue.endsWith('-fourth') ) {
         numValue = 4;
-    } else if (stringValue.endsWith('-fifth')) {
+    } else if (stringValue.endsWith('-fifth') ) {
         numValue = 5;
     } else {
         numValue = 0;
@@ -73,9 +73,30 @@ criterias.forEach(criteria => {
     stars.forEach(star => {
         star.addEventListener('click', (event) => {
             handleRatings(event.target.id, criteria);
-            
+
         });
     });
 });
 
+
+
+
+var updateStarRatings = (criteria) => {
+    var stars = document.querySelectorAll(`.${criteria} .updated i`);
+    console.log(stars)
+    var value = form[criteria].value;
+    console.log(value)
+
+    stars.forEach((star, index) => {
+        if (index < value) {
+            star.classList.add('checked');
+        } else {
+            star.classList.remove('checked');
+        }
+    });
+};
+
+    criterias.forEach(criteria => {
+        updateStarRatings(criteria);
+    });
 
